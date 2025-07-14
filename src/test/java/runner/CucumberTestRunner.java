aa38e8ecbb9bf05677e3475e634b0f3d80a80fc0
Faisal <faisaljayadi88@gmail.com>
2025-07-14 11:29:31 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test/java/helper/Endpoint.java
    #	src/test/java/helper/Models.java
    #	src/test/java/pages/apiPage.java
    #	src/test/java/testDef/apiStep.java
    #	src/test/resources/features/api.feature

--- src/test/java/runner/CucumberTestRunner.java
+++ src/test/java/runner/CucumberTestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "testDef",
-        plugin = {"pretty", "html:target/cucumber-api-reports.html"},
+        plugin = {"pretty", "html:reports/cucumber-api-reports.html"},
         tags = "@api",
         monochrome = true
 )
